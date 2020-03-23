d15dcabbdebae98be527ab9b635d6b3bcd5c5ae1
Mark Brown <broonie@kernel.org>
2020-03-23 16:47:29 +0000

    arm64: bti: Document behaviour for dynamically linked binaries
    For dynamically linked binaries the interpreter is responsible for setting
    PROT_BTI on everything except itself. The dynamic linker needs to be aware
    of PROT_BTI, for example in order to avoid dropping that when marking
    executable pages read only after doing relocations, and doing everything
    in userspace ensures that we don't get any issues due to divergences in
    behaviour between the kernel and dynamic linker within a single executable.
    Add a comment indicating that this is intentional to the code to help
    people trying to understand what's going on.
    
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- arch/arm64/kernel/process.c
+++ arch/arm64/kernel/process.c
@@ -675,6 +675,11 @@ asmlinkage void __sched arm64_preempt_schedule_irq(void)
 int arch_elf_adjust_prot(int prot, const struct arch_elf_state *state,
 			 bool has_interp, bool is_interp)
 {
+	/*
+	 * For dynamicly linked executables the interpreter is
+	 * responsible for setting PROT_BTI on everything except
+	 * itself.
+	 */
 	if (is_interp != has_interp)
 		return prot;
 

8bc4338c8a9760f707e5977409a1661300618041
Mark Brown <broonie@kernel.org>
2021-02-04 18:46:04 +0000

    elf: Allow architectures to parse properties on the main executable
    Currently the ELF code only attempts to parse properties on the image
    that will start execution, either the interpreter or for statically linked
    executables the main executable. The expectation is that any property
    handling for the main executable will be done by the interpreter. This is
    a bit inconsistent since we do map the executable and is causing problems
    for the arm64 BTI support when used in conjunction with systemd's use of
    seccomp to implement MemoryDenyWriteExecute which stops the dynamic linker
    adjusting the permissions of executable segments.
    
    Allow architectures to handle properties for both the dynamic linker and
    main executable, adjusting arch_parse_elf_properties() to have an is_interp
    flag as with arch_elf_adjust_prot() and calling it for both the main
    executable and any intepreter.
    
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- fs/binfmt_elf.c
+++ fs/binfmt_elf.c
@@ -716,7 +716,7 @@ out:
  */
 
 static int parse_elf_property(const char *data, size_t *off, size_t datasz,
-			      struct arch_elf_state *arch,
+			      struct arch_elf_state *arch, bool is_interp,
 			      bool have_prev_type, u32 *prev_type)
 {
 	size_t o, step;
@@ -751,7 +751,8 @@ static int parse_elf_property(const char *data, size_t *off, size_t datasz,
 	*prev_type = pr->pr_type;
 
 	ret = arch_parse_elf_property(pr->pr_type, data + o,
-				      pr->pr_datasz, ELF_COMPAT, arch);
+				      pr->pr_datasz, ELF_COMPAT, is_interp,
+				      arch);
 	if (ret)
 		return ret;
 
@@ -764,7 +765,7 @@ static int parse_elf_property(const char *data, size_t *off, size_t datasz,
 #define NOTE_NAME_SZ (sizeof(GNU_PROPERTY_TYPE_0_NAME))
 
 static int parse_elf_properties(struct file *f, const struct elf_phdr *phdr,
-				struct arch_elf_state *arch)
+				bool is_interp, struct arch_elf_state *arch)
 {
 	union {
 		struct elf_note nhdr;
@@ -813,7 +814,8 @@ static int parse_elf_properties(struct file *f, const struct elf_phdr *phdr,
 	have_prev_type = false;
 	do {
 		ret = parse_elf_property(note.data, &off, datasz, arch,
-					 have_prev_type, &prev_type);
+					 is_interp, have_prev_type,
+					 &prev_type);
 		have_prev_type = true;
 	} while (!ret);
 
@@ -828,6 +830,7 @@ static int load_elf_binary(struct linux_binprm *bprm)
 	unsigned long error;
 	struct elf_phdr *elf_ppnt, *elf_phdata, *interp_elf_phdata = NULL;
 	struct elf_phdr *elf_property_phdata = NULL;
+	struct elf_phdr *interp_elf_property_phdata = NULL;
 	unsigned long elf_bss, elf_brk;
 	int bss_prot = 0;
 	int retval, i;
@@ -963,12 +966,11 @@ out_free_interp:
 			goto out_free_dentry;
 
 		/* Pass PT_LOPROC..PT_HIPROC headers to arch code */
-		elf_property_phdata = NULL;
 		elf_ppnt = interp_elf_phdata;
 		for (i = 0; i < interp_elf_ex->e_phnum; i++, elf_ppnt++)
 			switch (elf_ppnt->p_type) {
 			case PT_GNU_PROPERTY:
-				elf_property_phdata = elf_ppnt;
+				interp_elf_property_phdata = elf_ppnt;
 				break;
 
 			case PT_LOPROC ... PT_HIPROC:
@@ -979,10 +981,17 @@ out_free_interp:
 					goto out_free_dentry;
 				break;
 			}
+
+		retval = parse_elf_properties(interpreter,
+					      interp_elf_property_phdata,
+					      true, &arch_state);
+		if (retval)
+			goto out_free_dentry;
+
 	}
 
-	retval = parse_elf_properties(interpreter ?: bprm->file,
-				      elf_property_phdata, &arch_state);
+	retval = parse_elf_properties(bprm->file, elf_property_phdata,
+				      false, &arch_state);
 	if (retval)
 		goto out_free_dentry;
 

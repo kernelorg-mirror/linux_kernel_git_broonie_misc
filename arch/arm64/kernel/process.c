f34d6a42a272d84c27f75c9463d3c3f48fd216a4
Mark Brown <broonie@kernel.org>
2021-05-21 13:08:50 +0100

    arm64: Enable BTI for main executable as well as the interpreter
    Currently for dynamically linked ELF executables we only enable BTI for
    the interpreter, expecting the interpreter to do this for the main
    executable. This is a bit inconsistent since we do map main executable and
    is causing issues with systemd's MemoryDenyWriteExecute feature which is
    implemented using a seccomp filter which prevents setting PROT_EXEC on
    already mapped memory and lacks the context to be able to detect that
    memory is already mapped with PROT_EXEC.
    
    Resolve this by checking the BTI property for the main executable and
    enabling BTI if it is present when doing the initial mapping. This does
    mean that we may get more code with BTI enabled if running on a system
    without BTI support in the dynamic linker, this is expected to be a safe
    configuration and testing seems to confirm that. It also reduces the
    flexibility userspace has to disable BTI but it is expected that for cases
    where there are problems which require BTI to be disabled it is more likely
    that it will need to be disabled on a system level.
    
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- arch/arm64/kernel/process.c
+++ arch/arm64/kernel/process.c
@@ -744,19 +744,13 @@ asmlinkage void __sched arm64_preempt_schedule_irq(void)
 int arch_elf_adjust_prot(int prot, const struct arch_elf_state *state,
 			 bool has_interp, bool is_interp)
 {
-	/*
-	 * For dynamically linked executables the interpreter is
-	 * responsible for setting PROT_BTI on everything except
-	 * itself.
-	 */
-	if (is_interp != has_interp)
-		return prot;
+	if (prot & PROT_EXEC) {
+		if (state->flags & ARM64_ELF_INTERP_BTI && is_interp)
+			prot |= PROT_BTI;
 
-	if (!(state->flags & ARM64_ELF_BTI))
-		return prot;
-
-	if (prot & PROT_EXEC)
-		prot |= PROT_BTI;
+		if (state->flags & ARM64_ELF_EXEC_BTI && !is_interp)
+			prot |= PROT_BTI;
+	}
 
 	return prot;
 }

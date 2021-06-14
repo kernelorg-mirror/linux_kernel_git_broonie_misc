f903d05976a39cc525215c987185e1cae518e422
Mark Brown <broonie@kernel.org>
2021-06-14 18:58:26 +0100

    elf: Remove has_interp property from arch_parse_elf_property()
    Since all current users of arch_parse_elf_property() are able to treat the
    interpreter and main executable orthogonaly the has_interp argument is now
    redundant so remove it.
    
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- arch/arm64/include/asm/elf.h
+++ arch/arm64/include/asm/elf.h
@@ -262,7 +262,7 @@ struct arch_elf_state {
 
 static inline int arch_parse_elf_property(u32 type, const void *data,
 					  size_t datasz, bool compat,
-					  bool has_interp, bool is_interp,
+					  bool is_interp,
 					  struct arch_elf_state *arch)
 {
 	/* No known properties for AArch32 yet */

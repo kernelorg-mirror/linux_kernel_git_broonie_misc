39fee8aa07b0d0faa5a1b2360aa62371b732c273
Mark Brown <broonie@kernel.org>
2021-06-14 23:32:13 +0100

    elf: Remove has_interp property from arch_adjust_elf_prot()
    Since we have added an is_interp flag to arch_parse_elf_property() we can
    drop the has_interp flag from arch_elf_adjust_prot(), the only user was
    the arm64 code which no longer needs it and any future users will be able
    to use arch_parse_elf_properties() to determine if an interpreter is in
    use.
    
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Tested-by: Jeremy Linton <jeremy.linton@arm.com>
    Reviewed-By: Dave Martin <Dave.Martin@arm.com>

--- arch/arm64/kernel/process.c
+++ arch/arm64/kernel/process.c
@@ -703,7 +703,7 @@ core_initcall(tagged_addr_init);
 
 #ifdef CONFIG_BINFMT_ELF
 int arch_elf_adjust_prot(int prot, const struct arch_elf_state *state,
-			 bool has_interp, bool is_interp)
+			 bool is_interp)
 {
 	if ((prot & PROT_EXEC) &&
 	    (state->flags & arm64_elf_bti_flag(is_interp)))

0acfd2d5c8b6a579fd9798bd7cdc73ae48562fe3
Mark Brown <broonie@kernel.org>
2022-05-25 22:07:37 +0100

    arm64/fpsimd: Load FP state based on recorded data type
    Now that we are recording the type of floating point register state we
    are saving when we save it we can use that information when we load to
    decide which register state is required and bring the TIF_SVE state into
    sync with the loaded register state.
    
    The SME state detauls are already recorded directly in the saved
    SVCR and handled based on the information there.
    
    Since we are not changing any of the save paths there should be no
    functional change from this patch, further patches will make use of this
    to optimise and clarify the code.
    
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- arch/arm64/kernel/fpsimd.c
+++ arch/arm64/kernel/fpsimd.c
@@ -391,11 +391,38 @@ static void task_fpsimd_load(void)
 	WARN_ON(!system_supports_fpsimd());
 	WARN_ON(!have_cpu_fpsimd_context());
 
-	/* Check if we should restore SVE first */
-	if (IS_ENABLED(CONFIG_ARM64_SVE) && test_thread_flag(TIF_SVE)) {
-		sve_set_vq(sve_vq_from_vl(task_get_sve_vl(current)) - 1);
-		restore_sve_regs = true;
-		restore_ffr = true;
+	if (system_supports_sve()) {
+		switch (current->thread.fp_type) {
+		case FP_STATE_FPSIMD:
+			/* Stop tracking SVE for this task until next use. */
+			if (test_and_clear_thread_flag(TIF_SVE))
+				sve_user_disable();
+			break;
+		case FP_STATE_SVE:
+			/*
+			 * A thread with SVE state should either be in
+			 * streaming mode or already have SVE enabled.
+			 */
+			if (!thread_sm_enabled(&current->thread) &&
+			    !WARN_ON_ONCE(!test_and_set_thread_flag(TIF_SVE)))
+				sve_user_enable();
+
+			sve_set_vq(sve_vq_from_vl(task_get_sve_vl(current)) - 1);
+			restore_sve_regs = true;
+			restore_ffr = true;
+			break;
+		default:
+			/*
+			 * This should never happen, we should always
+			 * record what we saved when we save. We
+			 * always at least have the memory allocated
+			 * for FPSMID registers so try that and hope
+			 * for the best.
+			 */
+			WARN_ON_ONCE(1);
+			clear_thread_flag(TIF_SVE);
+			break;
+		}
 	}
 
 	/* Restore SME, override SVE register configuration if needed */

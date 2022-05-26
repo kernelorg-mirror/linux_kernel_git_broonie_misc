a6cace62c75c2decc3f267f486596cb4dec5c659
Mark Brown <broonie@kernel.org>
2022-05-26 19:07:00 +0100

    arm64/sve: Don't zero non-FPSIMD register state on syscall by default
    The documented syscall ABI specifies that the SVE state not shared with
    FPSIMD is undefined after a syscall. Currently we implement this by
    always flushing this register state to zero, ensuring consistent
    behaviour but introducing some overhead in the case where we can return
    directly to userspace without otherwise needing to update the register
    state. Take advantage of the flexibility offered by the documented ABI
    and instead leave the SVE registers untouched in the case where can
    return directly to userspace.
    
    Since this is a user visible change a new sysctl abi.sve_syscall_clear_regs
    is provided which will restore the current behaviour of flushing the
    unshared register state unconditionally when enabled. This can be
    enabled for testing or to work around problems with applications that
    have been relying on the current flushing behaviour.
    
    The sysctl is disabled by default since it is anticipated that the risk
    of disruption to userspace is low. As well as being within the
    documented ABI this new behaviour mirrors the standard function call ABI
    for SVE in the AAPCS which should mean that compiler generated code is
    unlikely to rely on the current behaviour, the main risk is from hand
    coded assembly which directly invokes syscalls. The new behaviour is
    also what is currently implemented by qemu user mode emulation.
    
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- arch/arm64/kernel/syscall.c
+++ arch/arm64/kernel/syscall.c
@@ -158,6 +158,40 @@ trace_exit:
 	syscall_trace_exit(regs);
 }
 
+
+static unsigned int sve_syscall_regs_clear;
+
+#ifdef CONFIG_ARM64_SVE
+/*
+ * Global sysctl to control if we force the SVE register state not
+ * shared with FPSIMD to be cleared on every syscall. If this is not
+ * enabled then we will leave the state unchanged unless we need to
+ * reload from memory (eg, after a context switch).
+ */
+
+static struct ctl_table sve_syscall_sysctl_table[] = {
+	{
+		.procname	= "sve_syscall_clear_regs",
+		.mode		= 0644,
+		.data		= &sve_syscall_regs_clear,
+		.maxlen		= sizeof(int),
+		.proc_handler	= proc_dointvec_minmax,
+		.extra1		= SYSCTL_ZERO,
+		.extra2		= SYSCTL_ONE,
+	},
+	{ }
+};
+
+static int __init sve_syscall_sysctl_init(void)
+{
+	if (!register_sysctl("abi", sve_syscall_sysctl_table))
+		return -EINVAL;
+	return 0;
+}
+
+core_initcall(sve_syscall_sysctl_init);
+#endif	/* CONFIG_ARM64_SVE */
+
 /*
  * As per the ABI exit SME streaming mode and clear the SVE state not
  * shared with FPSIMD on syscall entry.
@@ -183,7 +217,7 @@ static inline void fp_user_discard(void)
 	if (!system_supports_sve())
 		return;
 
-	if (test_thread_flag(TIF_SVE)) {
+	if (sve_syscall_regs_clear && test_thread_flag(TIF_SVE)) {
 		unsigned int sve_vq_minus_one;
 
 		sve_vq_minus_one = sve_vq_from_vl(task_get_sve_vl(current)) - 1;

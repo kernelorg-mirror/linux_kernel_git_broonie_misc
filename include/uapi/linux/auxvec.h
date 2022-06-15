e60609f98e1c150a3a240589cd77a15b6fbf812d
Mark Brown <broonie@kernel.org>
2022-06-15 15:01:59 +0100

    elf: Allow architectures to provide AT_HWCAP3
    In order to provide for architectures which have filled both AT_HWCAP and
    AT_HWCAP2 add support for AT_HWCAP3.
    
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- include/uapi/linux/auxvec.h
+++ include/uapi/linux/auxvec.h
@@ -30,6 +30,7 @@
 				 * differ from AT_PLATFORM. */
 #define AT_RANDOM 25	/* address of 16 random bytes */
 #define AT_HWCAP2 26	/* extension of AT_HWCAP */
+#define AT_HWCAP3 27	/* further extension of AT_HWCAP */
 
 #define AT_EXECFN  31	/* filename of program */
 

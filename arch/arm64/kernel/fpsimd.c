a086a60b965c294dd4a1cb384041a1f4f6d301f1
Mark Brown <broonie@kernel.org>
2022-08-23 22:28:25 +0100

    arm64/sme: Implement context switching for ZT0
    When the system supports SME2 the ZT0 register must be context switched as
    part of the floating point state. This register is stored immediately
    after ZA in memory and is only accessible when PSTATE.ZA is set so we
    handle it in the same functions we use to save and restore ZA.
    
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- arch/arm64/kernel/fpsimd.c
+++ arch/arm64/kernel/fpsimd.c
@@ -396,7 +396,8 @@ static void task_fpsimd_load(void)
 		write_sysreg_s(current->thread.svcr, SYS_SVCR);
 
 		if (thread_za_enabled(&current->thread))
-			za_load_state(current->thread.sme_state);
+			sme_load_state(current->thread.sme_state,
+				       system_supports_sme2());
 
 		if (thread_sm_enabled(&current->thread)) {
 			restore_sve_regs = true;
@@ -449,7 +450,8 @@ static void fpsimd_save(void)
 		*svcr = read_sysreg_s(SYS_SVCR);
 
 		if (*svcr & SVCR_ZA_MASK)
-			za_save_state(last->sme_state);
+			sme_save_state(last->sme_state,
+				       system_supports_sme2());
 
 		/* If we are in streaming mode override regular SVE. */
 		if (*svcr & SVCR_SM_MASK) {

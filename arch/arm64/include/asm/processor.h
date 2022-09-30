3d9668fe5ed1cf174382742f83c1063f2e1b7a0e
Mark Brown <broonie@kernel.org>
2022-09-30 14:26:51 +0100

    arm64/sme: Rename za_state to sme_state
    In preparation for adding support for storage for ZT0 to the thread_struct
    rename za_state to sme_state. Since ZT0 is accessible when PSTATE.ZA is
    set just like ZA itself we will extend the allocation done for ZA to
    cover it, avoiding the need to further expand task_struct for non-SME
    tasks.
    
    No functional changes.
    
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- arch/arm64/include/asm/processor.h
+++ arch/arm64/include/asm/processor.h
@@ -154,7 +154,7 @@ struct thread_struct {
 
 	unsigned int		fpsimd_cpu;
 	void			*sve_state;	/* SVE registers, if any */
-	void			*za_state;	/* ZA register, if any */
+	void			*sme_state;	/* ZA and ZT state, if any */
 	unsigned int		vl[ARM64_VEC_MAX];	/* vector length */
 	unsigned int		vl_onexec[ARM64_VEC_MAX]; /* vl after next exec */
 	unsigned long		fault_address;	/* fault info */

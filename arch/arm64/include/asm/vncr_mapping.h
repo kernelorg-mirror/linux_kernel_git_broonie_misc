e745b10e27aa5cb1d1e0b0ce1147d9dfba926b54
Mark Brown <broonie@kernel.org>
2023-03-08 00:40:28 +0000

    KVM: arm64: Manage GCS access and registers for guests
    GCS introduces a number of system registers, on systems with GCS we need
    to context switch them and expose them to VMMs to allow guests to use
    GCS.
    
    In order to allow guests to use GCS we also need to configure
    HCRX_EL2.GCSEn, if this is not set GCS instructions will be noops and
    CHKFEAT will report GCS as disabled.
    
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- arch/arm64/include/asm/vncr_mapping.h
+++ arch/arm64/include/asm/vncr_mapping.h
@@ -97,6 +97,8 @@
 #define VNCR_PMSNEVFR_EL1       0x850
 #define VNCR_PMSDSFR_EL1        0x858
 #define VNCR_TRFCR_EL1          0x880
+#define VNCR_GCSPR_EL1		0x8C0
+#define VNCR_GCSCR_EL1		0x8D0
 #define VNCR_MPAM1_EL1          0x900
 #define VNCR_MPAMHCR_EL2        0x930
 #define VNCR_MPAMVPMV_EL2       0x938

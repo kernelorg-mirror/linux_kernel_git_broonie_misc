407cdb91f6d46e60e7920208099b07fabf366ade
Mark Brown <broonie@kernel.org>
2023-04-03 18:29:17 +0100

    regmap: Use mas_walk() instead of mas_find()
    Liam recommends using mas_walk() instead of mas_find() for our use case so
    let's do that, it avoids some minor overhead associated with being able to
    restart the operation which we don't need since we do a simple search.
    
    Suggested-by: Liam R. Howlett <Liam.Howlett@Oracle.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- drivers/base/regmap/regcache-maple.c
+++ drivers/base/regmap/regcache-maple.c
@@ -22,7 +22,9 @@ static int regcache_maple_read(struct regmap *map,
 
 	rcu_read_lock();
 
-	entry = mas_find(&mas, reg);
+	mas.index = reg;
+	mas.last = reg;
+	entry = mas_walk(&mas);
 	if (!entry) {
 		rcu_read_unlock();
 		return -ENOENT;
@@ -47,7 +49,9 @@ static int regcache_maple_write(struct regmap *map, unsigned int reg,
 
 	rcu_read_lock();
 
-	entry = mas_find(&mas, reg);
+	mas.index = reg;
+	mas.last = reg;
+	entry = mas_walk(&mas);
 	if (entry) {
 		entry[reg - mas.index] = val;
 		rcu_read_unlock();

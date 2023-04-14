26b0050d807bd4ee882cc8806f97eefe780022b0
Mark Brown <broonie@kernel.org>
2023-04-14 20:29:18 +0100

    mm: Define VM_SHADOW_STACK for arm64 when we support GCS
    Use VM_HIGH_ARCH_5 for guarded control stack pages.
    
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -372,7 +372,17 @@ extern unsigned int kobjsize(const void *objp);
  * having a PAGE_SIZE guard gap.
  */
 # define VM_SHADOW_STACK	VM_HIGH_ARCH_5
-#else
+#endif
+
+#if defined(CONFIG_ARM64_GCS)
+/*
+ * arm64's Guarded Control Stack implements similar functionality and
+ * has similar constraints to shadow stacks.
+ */
+# define VM_SHADOW_STACK	VM_HIGH_ARCH_5
+#endif
+
+#ifndef VM_SHADOW_STACK
 # define VM_SHADOW_STACK	VM_NONE
 #endif
 

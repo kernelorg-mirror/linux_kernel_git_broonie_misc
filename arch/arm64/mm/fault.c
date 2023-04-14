382af6d08706517271b0bb452416dd557699cbeb
Mark Brown <broonie@kernel.org>
2023-04-14 21:55:59 +0100

    arm64/esr: Add decode of ISS2 to data abort reporting
    The architecture has added more information about faults to ISS2 within
    ESR. Add decode of this to our data abort fault decode to aid diagnostics.
    Features that are not currently enabled are included here for completeness.
    
    Since the architecture specifies the values of bits within ISS2 in terms
    of ISS2 rather than in terms of the register as a whole we do so for our
    definitions as well, this makes it easier to review bitfield definitions.
    
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- arch/arm64/mm/fault.c
+++ arch/arm64/mm/fault.c
@@ -66,6 +66,8 @@ static inline const struct fault_info *esr_to_debug_fault_info(unsigned long esr
 
 static void data_abort_decode(unsigned long esr)
 {
+	u64 iss2 = ESR_ELx_ISS2(esr);
+
 	pr_alert("Data abort info:\n");
 
 	if (esr & ESR_ELx_ISV) {
@@ -81,9 +83,17 @@ static void data_abort_decode(unsigned long esr)
 		pr_alert("  ISV = 0, ISS = 0x%08lx\n", esr & ESR_ELx_ISS_MASK);
 	}
 
-	pr_alert("  CM = %lu, WnR = %lu\n",
+	pr_alert("  CM = %lu, WnR = %lu, TnD = %llu, TagAccess = %lld\n",
 		 (esr & ESR_ELx_CM) >> ESR_ELx_CM_SHIFT,
-		 (esr & ESR_ELx_WNR) >> ESR_ELx_WNR_SHIFT);
+		 (esr & ESR_ELx_WNR) >> ESR_ELx_WNR_SHIFT,
+		 (iss2 & ESR_ELx_TnD) >> ESR_ELx_TnD_SHIFT,
+		 (iss2 & ESR_ELx_TagAccess) >> ESR_ELx_TagAccess_SHIFT);
+
+	pr_alert("  GCS = %lld, Overlay = %llu, DirtyBit = %llu, Xs = %llu\n",
+		 (iss2 & ESR_ELx_GCS) >> ESR_ELx_GCS_SHIFT,
+		 (iss2 & ESR_ELx_Overlay) >> ESR_ELx_Overlay_SHIFT,
+		 (iss2 & ESR_ELx_DirtyBit) >> ESR_ELx_DirtyBit_SHIFT,
+		 (iss2 & ESR_ELx_Xs_MASK) >> ESR_ELx_Xs_SHIFT);
 }
 
 static void mem_abort_decode(unsigned long esr)

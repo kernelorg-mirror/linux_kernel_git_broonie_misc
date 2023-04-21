724d302701005a1f377444440ac53290e9a7fb9d
Mark Brown <broonie@kernel.org>
2023-04-21 20:53:01 +0100

    arm64/mm: Map pages for guarded control stack
    Map pages flagged as being part of a GCS as such rather than using the
    full set of generic VM flags.
    
    This is done using a conditional rather than extending the size of
    protection_map since that would make for a very sparse array.
    
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- arch/arm64/include/asm/mman.h
+++ arch/arm64/include/asm/mman.h
@@ -61,6 +61,12 @@ static inline bool arch_validate_flags(unsigned long vm_flags)
 			return false;
 	}
 
+	if (system_supports_gcs() && (vm_flags & VM_SHADOW_STACK)) {
+		/* An executable GCS isn't a good idea */
+		if (vm_flags & (VM_EXEC | VM_ARM64_BTI))
+			return false;
+	}
+
 	return true;
 
 }

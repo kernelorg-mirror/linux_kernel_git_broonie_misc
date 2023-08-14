c952b06a83e224c2f2fb2ce301eadbfdebad69c9
Mark Brown <broonie@kernel.org>
2023-08-14 17:54:15 +0100

    arm64/ptrace: Ensure that the task sees ZT writes on first use
    When the value of ZT is set via ptrace we don't disable traps for SME.
    This means that when a the task has never used SME before then the value
    set via ptrace will never be seen by the target task since it will
    trigger a SME access trap which will flush the register state.
    
    Disable SME traps when setting ZT, this means we also need to allocate
    storage for SVE if it is not already allocated, for the benefit of
    streaming SVE.
    
    Fixes: f90b529bcbe5 ("arm64/sme: Implement ZT0 ptrace support")
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- arch/arm64/kernel/ptrace.c
+++ arch/arm64/kernel/ptrace.c
@@ -1170,6 +1170,11 @@ static int zt_set(struct task_struct *target,
 	if (!system_supports_sme2())
 		return -EINVAL;
 
+	/* Ensure SVE storage in case this is first use of SME */
+	sve_alloc(target, false);
+	if (!target->thread.sve_state)
+		return -ENOMEM;
+
 	if (!thread_za_enabled(&target->thread)) {
 		sme_alloc(target);
 		if (!target->thread.sme_state)
@@ -1181,6 +1186,8 @@ static int zt_set(struct task_struct *target,
 				 0, ZT_SIG_REG_BYTES);
 	if (ret == 0)
 		target->thread.svcr |= SVCR_ZA_MASK;
+
+	set_tsk_thread_flag(target, TIF_SME);
 
 	fpsimd_flush_task_state(target);
 

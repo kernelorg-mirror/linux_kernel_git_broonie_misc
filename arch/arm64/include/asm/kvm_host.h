1d2813568c2ac8e954704149c5c8f629cfb1e77b
Mark Brown <broonie@kernel.org>
2023-09-19 15:16:40 +0100

    KVM: arm64: Implement SME vector length configuration
    SME implements a vector length which architecturally looks very similar
    to that for SVE, configured in a very similar manner.  This controls the
    vector length used for the ZA matrix register, and for the SVE vector
    and predicate registers when in streaming mode.  The only substantial
    difference is that unlike SVE the architecture does not guarantee that
    any particular vector length will be implemented.
    
    Configuration for SME vector lengths is done using a virtual register as
    for SVE, hook up the implementation for the virtual register.  Since we
    do not yet have support for any of the new SME registers stub register
    access functions are provided that only allow VL configuration.  These
    will be extended as the SME specific registers, as for SVE.
    
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- arch/arm64/include/asm/kvm_host.h
+++ arch/arm64/include/asm/kvm_host.h
@@ -708,8 +708,15 @@ struct kvm_vcpu_arch {
 	 * low 128 bits of the SVE Z registers.  When the core
 	 * floating point code saves the register state of a task it
 	 * records which view it saved in fp_type.
+	 *
+	 * If SME support is also present then it provides an
+	 * alternative view of the SVE registers accessed as for the Z
+	 * registers when PSTATE.SM is 1, plus an additional set of
+	 * SME specific state in the matrix register ZA and LUT
+	 * register ZT0.
 	 */
 	void *sve_state;
+	void *sme_state;
 	enum fp_type fp_type;
 	unsigned int max_vl[ARM64_VEC_MAX];
 

09df5d251b579f988af54af67cf08d4e50f10883
Mark Brown <broonie@kernel.org>
2023-10-16 19:40:40 +0100

    fork: Add shadow stack support to clone3()
    Unlike with the normal stack there is no API for configuring the the shadow
    stack for a new thread, instead the kernel will dynamically allocate a new
    shadow stack with the same size as the normal stack. This appears to be due
    to the shadow stack series having been in development since before the more
    extensible clone3() was added rather than anything more deliberate.
    
    Add parameters to clone3() specifying the location and size of a shadow
    stack for the newly created process.  If no shadow stack is specified
    then the existing implicit allocation behaviour is maintained.
    
    If a stack is specified then it is required to have an architecture
    defined token placed on the stack, this will be consumed by the new
    task.  If the token is not provided then this will be reported as a
    segmentation fault with si_code SEGV_CPERR, as a runtime shadow stack
    protection error would be.  This allows architectures to implement the
    validation of the token in the child process context.
    
    If the architecture does not support shadow stacks the shadow stack
    parameters must be zero, architectures that do support the feature are
    expected to enforce the same requirement on individual systems that lack
    shadow stack support.
    
    Update the existing x86 implementation to pay attention to the newly added
    arguments, in order to maintain compatibility we use the existing behaviour
    if no shadow stack is specified. Minimal validation is done of the supplied
    parameters, detailed enforcement is left to when the thread is executed.
    Since we are now using more fields from the kernel_clone_args we pass that
    into the shadow stack code rather than individual fields.
    
    At present this implementation does not consume the shadow stack token
    atomically as would be desirable, it uses a separate read and write.
    
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- arch/x86/kernel/shstk.c
+++ arch/x86/kernel/shstk.c
@@ -191,44 +191,103 @@ void reset_thread_features(void)
 	current->thread.features_locked = 0;
 }
 
-unsigned long shstk_alloc_thread_stack(struct task_struct *tsk, unsigned long clone_flags,
-				       unsigned long stack_size)
+int arch_shstk_validate_clone(struct task_struct *t,
+			      struct vm_area_struct *vma,
+			      struct page *page,
+			      struct kernel_clone_args *args)
+{
+	/*
+	 * SSP is aligned, so reserved bits and mode bit are a zero, just mark
+	 * the token 64-bit.
+	 */
+	void *maddr = kmap_local_page(page);
+	int offset;
+	unsigned long addr, ssp;
+	u64 expected;
+	u64 val;
+
+	if (!features_enabled(ARCH_SHSTK_SHSTK))
+		return 0;
+
+	ssp = args->shadow_stack + args->shadow_stack_size;
+	addr = ssp - SS_FRAME_SIZE;
+	expected = ssp | BIT(0);
+	offset = offset_in_page(ssp);
+
+	/* This should really be an atomic cmpxchg.  It is not. */
+	copy_from_user_page(vma, page, addr, &val, maddr + offset,
+			    sizeof(val));
+
+	if (val != expected)
+		return false;
+	val = 0;
+
+	copy_to_user_page(vma, page, addr, maddr + offset, &val, sizeof(val));
+	set_page_dirty_lock(page);
+
+	return 0;
+}
+
+unsigned long shstk_alloc_thread_stack(struct task_struct *tsk,
+				       const struct kernel_clone_args *args)
 {
 	struct thread_shstk *shstk = &tsk->thread.shstk;
+	unsigned long clone_flags = args->flags;
 	unsigned long addr, size;
 
 	/*
 	 * If shadow stack is not enabled on the new thread, skip any
-	 * switch to a new shadow stack.
+	 * implicit switch to a new shadow stack and reject attempts to
+	 * explicitly specify one.
 	 */
-	if (!features_enabled(ARCH_SHSTK_SHSTK))
-		return 0;
+	if (!features_enabled(ARCH_SHSTK_SHSTK)) {
+		if (args->shadow_stack || args->shadow_stack_size)
+			return (unsigned long)ERR_PTR(-EINVAL);
 
-	/*
-	 * For CLONE_VFORK the child will share the parents shadow stack.
-	 * Make sure to clear the internal tracking of the thread shadow
-	 * stack so the freeing logic run for child knows to leave it alone.
-	 */
-	if (clone_flags & CLONE_VFORK) {
-		shstk->base = 0;
-		shstk->size = 0;
 		return 0;
 	}
 
 	/*
-	 * For !CLONE_VM the child will use a copy of the parents shadow
-	 * stack.
+	 * If the user specified a shadow stack then do some basic
+	 * validation and use it, otherwise fall back to a default
+	 * shadow stack size if the clone_flags don't indicate an
+	 * allocation is unneeded.
 	 */
-	if (!(clone_flags & CLONE_VM))
-		return 0;
+	if (args->shadow_stack) {
+		addr = args->shadow_stack;
+		size = args->shadow_stack_size;
+		shstk->base = 0;
+		shstk->size = 0;
+	} else {
+		/*
+		 * For CLONE_VFORK the child will share the parents
+		 * shadow stack.  Make sure to clear the internal
+		 * tracking of the thread shadow stack so the freeing
+		 * logic run for child knows to leave it alone.
+		 */
+		if (clone_flags & CLONE_VFORK) {
+			shstk->base = 0;
+			shstk->size = 0;
+			return 0;
+		}
 
-	size = adjust_shstk_size(stack_size);
-	addr = alloc_shstk(0, size, 0, false);
-	if (IS_ERR_VALUE(addr))
-		return addr;
+		/*
+		 * For !CLONE_VM the child will use a copy of the
+		 * parents shadow stack.
+		 */
+		if (!(clone_flags & CLONE_VM))
+			return 0;
 
-	shstk->base = addr;
-	shstk->size = size;
+		size = args->stack_size;
+		size = adjust_shstk_size(size);
+		addr = alloc_shstk(0, size, 0, false);
+		if (IS_ERR_VALUE(addr))
+			return addr;
+
+		/* We allocated the shadow stack, we should deallocate it. */
+		shstk->base = addr;
+		shstk->size = size;
+	}
 
 	return addr + size;
 }

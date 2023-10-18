99c82a3ff5b6604c7dbad61f075664a176857561
Mark Brown <broonie@kernel.org>
2023-10-18 23:09:49 +0100

    kselftest/clone3: Test shadow stack support
    Add basic test coverage for specifying the shadow stack for a newly
    created thread via clone3(), including coverage of the newly extended
    argument structure.
    
    In order to facilitate testing on systems without userspace shadow stack
    support we manually enable shadow stacks on startup, this is architecture
    specific due to the use of an arch_prctl() on x86. Due to interactions with
    potential userspace locking of features we actually detect support for
    shadow stacks on the running system by attempting to allocate a shadow
    stack page during initialisation using map_shadow_stack(), warning if this
    succeeds when the enable failed.
    
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- tools/testing/selftests/clone3/clone3_selftests.h
+++ tools/testing/selftests/clone3/clone3_selftests.h
@@ -31,6 +31,13 @@ struct __clone_args {
 	__aligned_u64 set_tid;
 	__aligned_u64 set_tid_size;
 	__aligned_u64 cgroup;
+#ifndef CLONE_ARGS_SIZE_VER2
+#define CLONE_ARGS_SIZE_VER2 88	/* sizeof third published struct */
+#endif
+	__aligned_u64 shadow_stack_size;
+#ifndef CLONE_ARGS_SIZE_VER3
+#define CLONE_ARGS_SIZE_VER3 96 /* sizeof fourth published struct */
+#endif
 };
 
 static pid_t sys_clone3(struct __clone_args *args, size_t size)

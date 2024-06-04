c1d16d2f1ce3c2ae62c6b5e74cdb15f8090a92af
Mark Brown <broonie@kernel.org>
2024-06-04 19:22:20 +0100

    arm64/fpsimd: Discover maximum vector length implemented by any CPU
    When discovering the vector lengths for SVE and SME we do not currently
    record the maximum VL supported on any individual CPU.  This is expected
    to be the same for all CPUs but the architecture allows asymmetry, if we
    do encounter an asymmetric system then some CPUs may support VLs higher
    than the maximum Linux will use.  Since the pKVM hypervisor needs to
    support saving and restoring anything the host can physically set it
    needs to know the maximum value any CPU could have, add support for
    enumerating it and validation for late CPUs.
    
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- arch/arm64/kernel/fpsimd.c
+++ arch/arm64/kernel/fpsimd.c
@@ -129,6 +129,7 @@ __ro_after_init struct vl_info vl_info[ARM64_VEC_MAX] = {
 		.min_vl			= SVE_VL_MIN,
 		.max_vl			= SVE_VL_MIN,
 		.max_virtualisable_vl	= SVE_VL_MIN,
+		.max_cpu_vl		= SVE_VL_MIN,
 	},
 #endif
 #ifdef CONFIG_ARM64_SME
@@ -1041,8 +1042,13 @@ static void vec_probe_vqs(struct vl_info *info,
 void __init vec_init_vq_map(enum vec_type type)
 {
 	struct vl_info *info = &vl_info[type];
+	unsigned long b;
+
 	vec_probe_vqs(info, info->vq_map);
 	bitmap_copy(info->vq_partial_map, info->vq_map, SVE_VQ_MAX);
+
+	b = find_first_bit(info->vq_map, SVE_VQ_MAX);
+	info->max_cpu_vl = __bit_to_vl(b);
 }
 
 /*
@@ -1054,11 +1060,16 @@ void vec_update_vq_map(enum vec_type type)
 {
 	struct vl_info *info = &vl_info[type];
 	DECLARE_BITMAP(tmp_map, SVE_VQ_MAX);
+	unsigned long b;
 
 	vec_probe_vqs(info, tmp_map);
 	bitmap_and(info->vq_map, info->vq_map, tmp_map, SVE_VQ_MAX);
 	bitmap_or(info->vq_partial_map, info->vq_partial_map, tmp_map,
 		  SVE_VQ_MAX);
+
+	b = find_first_bit(tmp_map, SVE_VQ_MAX);
+	if (__bit_to_vl(b) > info->max_cpu_vl)
+		info->max_cpu_vl = __bit_to_vl(b);
 }
 
 /*
@@ -1069,9 +1080,10 @@ int vec_verify_vq_map(enum vec_type type)
 {
 	struct vl_info *info = &vl_info[type];
 	DECLARE_BITMAP(tmp_map, SVE_VQ_MAX);
-	unsigned long b;
+	unsigned long b, max_vl;
 
 	vec_probe_vqs(info, tmp_map);
+	max_vl = __bit_to_vl(find_first_bit(tmp_map, SVE_VQ_MAX));
 
 	bitmap_complement(tmp_map, tmp_map, SVE_VQ_MAX);
 	if (bitmap_intersects(tmp_map, info->vq_map, SVE_VQ_MAX)) {
@@ -1082,6 +1094,18 @@ int vec_verify_vq_map(enum vec_type type)
 
 	if (!IS_ENABLED(CONFIG_KVM) || !is_hyp_mode_available())
 		return 0;
+
+	/*
+	 * pKVM allocates and uses storage for host state based on the
+	 * largest per-PE VL, reject new PEs with a larger maximum.
+	 */
+	if (is_protected_kvm_enabled()) {
+		if (max_vl > info->max_cpu_vl) {
+			pr_warn("%s: cpu%d: would increase maximum VL\n",
+				info->name, smp_processor_id());
+			return -EINVAL;
+		}
+	}
 
 	/*
 	 * For KVM, it is necessary to ensure that this CPU doesn't

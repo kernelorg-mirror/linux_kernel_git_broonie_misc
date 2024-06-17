89218566c84c50ee6dee8ddcaa65c849295fb280
Mark Brown <broonie@kernel.org>
2024-06-17 20:55:22 +0100

    arm64/fpsimd: Ensure we don't contend a SMCU from idling CPUs
    When we enter the kernel we currently don't update any of the floating
    point register state until either something else uses floating point or we
    get a CPU_PM_ENTER notification during suspend or cpuidle. This means that
    for a system which has been configured with both suspend and cpuidle
    disabled we will leave whatever floating point state was loaded in the
    registers present while a CPU is idling.
    
    For SME this may cause an idling CPU to interfere with the operation of
    other CPUs, SME may be implemented as a SMCU shared with between multiple
    CPUs. Leaving ZA or especially streaming mode enabled may be taken by
    the hardware as an indication that SME is active use by the CPU and cause
    resources to be allocated to it at the expense of other CPUs.
    
    Since we exit streaming mode on entering syscalls we are unlikely to be
    idling the CPU in streaming mode due to a blocking syscall, most likely
    it will be a task being scheduled to a different CPU or potentially
    fault handling.  The former will result in the loaded state being
    invalidated, the latter is a slow path anyway.  For ZA we do allow it to
    be enabled during syscalls so we could see a blocking syscall for that,
    although the strong recommendation is that it be disabled as much as
    possible so it should also be an uncommon case.
    
    Add an arch_cpu_idle_enter() implementation which disables SME if it is
    active when we idle the CPU, ensuring that we don't create any spurious
    contention even if cpuidle is not enabled.
    
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- arch/arm64/include/asm/fpsimd.h
+++ arch/arm64/include/asm/fpsimd.h
@@ -82,6 +82,8 @@ extern void fpsimd_restore_current_state(void);
 extern void fpsimd_update_current_state(struct user_fpsimd_state const *state);
 extern void fpsimd_kvm_prepare(void);
 
+extern void fpsimd_idle_enter(void);
+
 struct cpu_fp_state {
 	struct user_fpsimd_state *st;
 	void *sve_state;

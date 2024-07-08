4b2262cefe83beb03cc187aa403a936218eaafd8
Mark Brown <broonie@kernel.org>
2024-07-09 00:58:10 +0100

    KVM: arm64: Support SME priority registers
    SME has optional support for configuring the relative priorities of PEs
    in systems where they share a single SME hardware block, known as a
    SMCU. Currently we do not have any support for this in Linux and will
    also hide it from KVM guests, pending experience with practical
    implementations. The interface for configuring priority support is via
    two new system registers, these registers are always defined when SME is
    available.
    
    The register SMPRI_EL1 allows control of SME execution priorities. Since
    we disable SME priority support for guests this register is RES0, define
    it as such and enable fine grained traps for SMPRI_EL1 to ensure that
    guests can't write to it even if the hardware supports priorites.  Since
    the register should be readable with fixed contents we only trap writes,
    not reads. Since there is no host support for using priorities the
    register currently left with a value of 0 by the host so we do not need
    to update the value for guests.
    
    There is also an EL2 register SMPRIMAP_EL2 for virtualisation of
    priorities, this is RES0 when priority configuration is not supported
    but has no specific traps available.  When saving state from a nested
    guest we overwite any value the guest stored.
    
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- arch/arm64/kvm/config.c
+++ arch/arm64/kvm/config.c
@@ -1481,6 +1481,9 @@ static void __compute_hfgwtr(struct kvm_vcpu *vcpu)
 
 	if (cpus_have_final_cap(ARM64_WORKAROUND_AMPERE_AC03_CPU_38))
 		*vcpu_fgt(vcpu, HFGWTR_EL2) |= HFGWTR_EL2_TCR_EL1;
+
+	if (kvm_has_feat(vcpu->kvm, ID_AA64PFR1_EL1, SME, IMP))
+		*vcpu_fgt(vcpu, HFGWTR_EL2) |= HFGWTR_EL2_nSMPRI_EL1;
 }
 
 static void __compute_hdfgwtr(struct kvm_vcpu *vcpu)

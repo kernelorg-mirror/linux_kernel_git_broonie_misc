e51bfe9ba90638cb73d94de09ec39477fba004ce
Mark Brown <broonie@kernel.org>
2024-12-10 20:12:39 +0000

    arm64/fpsimd: Update FA64 and ZT0 enables when loading SME state
    Currently we enable EL0 and EL1 access to FA64 and ZT0 at boot and leave
    them enabled throughout the runtime of the system. When we add KVM support
    we will need to make this configuration dynamic, these features may be
    disabled for some KVM guests. Since the host kernel saves the floating
    point state for non-protected guests and we wish to avoid KVM having to
    reload the floating point state needlessly on guest reentry let's move the
    configuration of these enables to the floating point state reload.
    
    We provide a helper which does the configuration as part of a
    read/modify/write operation along with the configuration of the task VL,
    then update the floating point state load and SME access trap to use it.
    We also remove the setting of the enable bits from the CPU feature
    identification and resume paths.  There will be a small overhead from
    setting the enables one at a time but this should be negligable in the
    context of the state load or access trap.  In order to avoid compiler
    warnings due to unused variables in !CONFIG_ARM64_SME cases we avoid
    storing the vector length in temporary variables.
    
    Signed-off-by: Mark Brown <broonie@kernel.org>
    
    df

--- arch/arm64/include/asm/fpsimd.h
+++ arch/arm64/include/asm/fpsimd.h
@@ -402,6 +402,18 @@ static inline size_t sme_state_size(struct task_struct const *task)
 	return size;
 }
 
+#define sme_cond_update_smcr(vl, fa64, zt0, reg)		\
+	do {							\
+		u64 __old = read_sysreg_s((reg));		\
+		u64 __new = vl;					\
+		if (fa64)			\
+			__new |= SMCR_ELx_FA64;			\
+		if (zt0)					\
+			__new |= SMCR_ELx_EZT0;			\
+		if (__old != __new)				\
+			write_sysreg_s(__new, (reg));		\
+	} while (0)
+
 #else
 
 static inline void sme_user_disable(void) { BUILD_BUG(); }
@@ -424,6 +436,8 @@ static inline size_t sme_state_size(struct task_struct const *task)
 {
 	return 0;
 }
+
+#define sme_cond_update_smcr(val, fa64, zt0, reg) do { } while (0)
 
 #endif /* ! CONFIG_ARM64_SME */
 

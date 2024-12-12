60befc7755d4a7baa0d25254d6a737caad24e3ac
Mark Brown <broonie@kernel.org>
2024-12-12 19:31:47 +0000

    arm64/fpsimd: Check enable bit for FA64 when saving EFI state
    Currently when deciding if we need to save FFR when in streaming mode prior
    to EFI calls we check if FA64 is supported by the system. Since KVM guest
    support will mean that FA64 might be enabled and disabled at runtime switch
    to checking if traps for FA64 are enabled in SMCR_EL1 instead.
    
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- arch/arm64/kernel/fpsimd.c
+++ arch/arm64/kernel/fpsimd.c
@@ -1913,6 +1913,11 @@ static DEFINE_PER_CPU(bool, efi_sm_state);
  * either doing something wrong or you need to propose some refactoring.
  */
 
+static bool fa64_enabled(void)
+{
+	return read_sysreg_s(SYS_SMCR_EL1) & SMCR_ELx_FA64;
+}
+
 /*
  * __efi_fpsimd_begin(): prepare FPSIMD for making an EFI runtime services call
  */
@@ -1947,7 +1952,7 @@ void __efi_fpsimd_begin(void)
 				 * Unless we have FA64 FFR does not
 				 * exist in streaming mode.
 				 */
-				if (!system_supports_fa64())
+				if (!fa64_enabled())
 					ffr = !(svcr & SVCR_SM_MASK);
 			}
 
@@ -1998,7 +2003,7 @@ void __efi_fpsimd_end(void)
 					 * Unless we have FA64 FFR does not
 					 * exist in streaming mode.
 					 */
-					if (!system_supports_fa64())
+					if (!fa64_enabled())
 						ffr = false;
 				}
 			}

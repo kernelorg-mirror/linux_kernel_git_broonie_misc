387d0a6de63a92c49aebe053ca90cf14ea290c7f
Mark Brown <broonie@kernel.org>
2025-02-12 00:24:36 +0000

    KVM: arm64: Expose SME specific state to userspace
    SME introduces two new registers, the ZA matrix register and the ZT0 LUT
    register.  Both of these registers are only accessible when PSTATE.ZA is
    set and ZT0 is only present if SME2 is enabled for the guest. Provide
    support for configuring these from VMMs.
    
    The ZA matrix is a single SVL*SVL register which is available when
    PSTATE.ZA is set. We follow the pattern established by the architecture
    itself and expose this to userspace as a series of horizontal SVE vectors
    with the streaming mode vector length, using the format already established
    for the SVE vectors themselves.
    
    ZT0 is a single register with a refreshingly fixed size 512 bit register
    which is like ZA accessible only when PSTATE.ZA is set. Add support for it
    to the userspace API, as with ZA we allow the register to be read or written
    regardless of the state of PSTATE.ZA in order to simplify userspace usage.
    The value will be reset to 0 whenever PSTATE.ZA changes from 0 to 1,
    userspace can read stale values but these are not observable by the guest
    without manipulation of PSTATE.ZA by userspace.
    
    While there is currently only one ZT register the naming as ZT0 and the
    instruction encoding clearly leave room for future extensions adding more
    ZT registers. This encoding can readily support such an extension if one is
    introduced.
    
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- arch/arm64/include/asm/kvm_host.h
+++ arch/arm64/include/asm/kvm_host.h
@@ -1020,6 +1020,24 @@ struct kvm_vcpu_arch {
 	__size_ret;							\
 })
 
+#define vcpu_sme_state(vcpu) (kern_hyp_va((vcpu)->arch.sme_state))
+
+#define vcpu_sme_state_size(vcpu) ({					\
+	size_t __size_ret;						\
+	unsigned int __vcpu_vq;						\
+									\
+	if (WARN_ON(!sve_vl_valid((vcpu)->arch.max_vl[ARM64_VEC_SME]))) { \
+		__size_ret = 0;						\
+	} else {							\
+		__vcpu_vq = vcpu_sme_max_vq(vcpu);			\
+		__size_ret = ZA_SIG_REGS_SIZE(__vcpu_vq);		\
+		if (system_supports_sme2())				\
+			__size_ret += ZT_SIG_REG_SIZE;			\
+	}								\
+									\
+	__size_ret;							\
+})
+
 /*
  * Only use __vcpu_sys_reg/ctxt_sys_reg if you know you want the
  * memory backed version of a register, and not the one most recently
@@ -1602,5 +1620,8 @@ void kvm_set_vm_id_reg(struct kvm *kvm, u32 reg, u64 val);
 
 #define vcpu_in_streaming_mode(vcpu) \
 	(__vcpu_sys_reg(vcpu, SVCR) & SVCR_SM_MASK)
+
+#define vcpu_za_enabled(vcpu) \
+	(__vcpu_sys_reg(vcpu, SVCR) & SVCR_ZA_MASK)
 
 #endif /* __ARM64_KVM_HOST_H__ */

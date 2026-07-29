3f9428183fb252d367cefccd6d99333fb15bce62
Mark Brown <broonie@kernel.org>
2026-07-29 17:43:39 +0100

    KVM: arm64: Block ID register changes after we rely on the values
    In commit c5bac1ef7df6b ("KVM: arm64: Move existing feature disabling
    over to FGU infrastructure") a check was added to suppress duplicate
    recalculation of FGUs based on a flag KVM_ARCH_FLAG_FGU_INITIALIZED. This
    flag is set when we complete kvm_calculate_traps(), which is called from
    kvm_arch_vcpu_run_pid_change(). There are several points where that
    function could fail after we have calculated FGUs (eg, due to an invalid
    timer configuration). If this happens then userspace will still be able
    to write to the ID registers, writes to which are gated on
    KVM_ARCH_FLAG_HAS_RAN_ONCE being set. This in turn means that the FGU
    configuration for a running guest may not match the ID register
    configuration.
    
    This will result in issues based on the hypervisor assuming a consistent
    configuration, for example it allows the creation of guests which have
    untrapped access to system registers which are not context switched for
    the guest.
    
    A similar issue exists in kvm_init_nv_sysregs() where once sysreg_masks
    is allocated the RES0/RES1 masks for registers are fixed based on the ID
    register values at the time the function ran, and also for copying the
    implementation ID registers to the hypervisor for pKVM.
    
    There is a further issue with vGIC setup, creating a vGIC includes
    updating the ID registers to reflect the GIC configuration. We refuse
    to create a vGIC after the first vCPU has run but if a vCPU fails its
    first run we may already have finalized the ID register values.
    
    Avoid these issues by adding a new flag that we set when we finalize the
    system registers, blocking ID register changes after that has been set
    even if something fails later on. Do this in kvm_vm_finalize_sys_regs(),
    this is where we finalize the GIC fields in the ID registers and happens
    before we do the FGU and RES0/1 setup. A VMM which tries to create an
    irqchip after failing to run a vCPU will now get -EBUSY rather than a
    likely misconfigured guest. Userspace is not expected to try to run a
    guest that fails to start, never mind try to repair the guest
    configuration after doing so, so this is not expected to have any impact
    on practical users.
    
    Fixes: c5bac1ef7df6b ("KVM: arm64: Move existing feature disabling over to FGU infrastructure")
    Fixes: 888f088070229 ("KVM: arm64: nv: Add sanitising to VNCR-backed sysregs")
    Fixes: 03e1b89d051f ("KVM: arm64: Copy MIDR_EL1 into hyp VM when it is writable")
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- arch/arm64/kvm/sys_regs.c
+++ arch/arm64/kvm/sys_regs.c
@@ -2427,9 +2427,10 @@ static int set_id_reg(struct kvm_vcpu *vcpu, const struct sys_reg_desc *rd,
 
 	/*
 	 * Once the VM has started the ID registers are immutable. Reject any
-	 * write that does not match the final register value.
+	 * write that does not match the final register value once we have
+	 * got far enough into first running the VM to use the values.
 	 */
-	if (kvm_vm_has_ran_once(vcpu->kvm)) {
+	if (vcpu_id_regs_final(vcpu)) {
 		if (val != read_id_reg(vcpu, rd))
 			ret = -EBUSY;
 		else
@@ -2463,7 +2464,7 @@ void kvm_set_vm_id_reg(struct kvm *kvm, u32 reg, u64 val)
 
 	lockdep_assert_held(&kvm->arch.config_lock);
 
-	if (KVM_BUG_ON(kvm_vm_has_ran_once(kvm) || !p, kvm))
+	if (KVM_BUG_ON(kvm_id_regs_final(kvm) || !p, kvm))
 		return;
 
 	*p = val;
@@ -3149,10 +3150,10 @@ static int set_imp_id_reg(struct kvm_vcpu *vcpu, const struct sys_reg_desc *r,
 		return -EINVAL;
 
 	/*
-	 * Once the VM has started the ID registers are immutable. Reject the
-	 * write if userspace tries to change it.
+	 * Once we have been far enough into starting the VM the ID registers
+	 * are immutable. Reject the write if userspace tries to change it.
 	 */
-	if (kvm_vm_has_ran_once(kvm))
+	if (kvm_id_regs_final(kvm))
 		return -EBUSY;
 
 	/*
@@ -5763,7 +5764,7 @@ out:
  */
 static int kvm_vm_finalize_sys_regs(struct kvm *kvm)
 {
-	if (kvm_vm_has_ran_once(kvm))
+	if (kvm_id_regs_final(kvm))
 		return 0;
 
 	/*
@@ -5810,6 +5811,8 @@ static int kvm_vm_finalize_sys_regs(struct kvm *kvm)
 		 */
 		kvm_vgic_finalize_idregs(kvm);
 	}
+
+	set_bit(KVM_ARCH_FLAG_ID_REGS_FINAL, &kvm->arch.flags);
 
 	return 0;
 }
